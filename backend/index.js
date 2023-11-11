cc92341c6d6cff87da11acc74275036db88ee4e4
Shokhrukh Ibragimov <dotanoobbich@mail.ru>
2023-11-11 18:54:02 -0500

    Implement Azure Blob Storage file upload functionality

--- backend/index.js
+++ backend/index.js
@@ -7,25 +7,31 @@ require("dotenv").config();
 
 // API for working with Azure Blob Storage
 const { BlobServiceClient } = require("@azure/storage-blob");
+// initialize BlobServiceClient -> instance for using methods on service level
 const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.BLOB_CONNECTION_STRING);
-
-
+const containerName = "images";
+// object that provides methods to interact with a specific container('imgages') in the Blob Storage service
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
 // we use multer to handle "multipart/form-data" which is required for file uploads
 const multer = require("multer");
 const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Set the destination for file uploads
-  },
-  filename: function (req, file, cb) {
-    // Create a new filename with the original name and extension
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  },
+// We use buffer, because our images our small
+// Which makes it easier to save the file locally and then streamline it to azure blob storage
+// If the file is bigger than 4MB, it is easier to send it to azure blob storage directly 
+// we also refer to buffer as temporary storage, as it is not used for large files 
+// shortly, мы пакуем картинку локально, и отсылаем буфер в азюр блоб
+const upload = multer({ 
+  // store buffer in memory until next process
+  storage: multer.memoryStorage(),
+  // we don't want our file to be too large
+  limits: { 
+    fileSize: 4 * 1024 * 1024, // Limit file size to 4MB
+  }
 });
 
-const upload = multer({ storage: storage });
+
 
 // express instance is initiated, и он развёртывает объект, методы которого мы можем использовать
 // in other words, express is a constructor, and app is an instance of that constructor
@@ -37,9 +43,27 @@ const corsOptions = {
   methods: ['POST'] // Only allow POST method from this origin
 };
 
+// our callback is async because we're involving azure blob storage which is async
+app.post('/upload/', cors(corsOptions), upload.single('file'), async (req,res)=>{
+  
+  if(!req.file) { 
+    return res.status(400).send("No file uploaded.");
+  }
 
-app.post('/upload/', cors(corsOptions), upload.single('file'), (req,res)=>{
-  res.status(200).json({ message: "File uploaded successfully" }); 
+  try { 
+    // here we're using the request that was sent by form in FormUpload
+    let blobName = "file-" + Date.now() + path.extname(req.file.originalname);
+    // here, we're creating an instance of BlobClient with name blobName
+    let blockBlobClient = containerClient.getBlockBlobClient(blobName); 
+    // actually uploading the file onto blob storage (fileData, fileSize)
+    // await actually pauses this code until we get success or error
+    await blockBlobClient.upload(req.file.buffer, req.file.size);
+    res.status(200).send({ message: "File uploaded to Azure Blob Storage." });
+  }catch(e) { 
+    console.log("Error:", e);
+    res.status(500).send("Error uploading file to Azure Blob Storage.");
+  }
+
 });
 
 const PORT = process.env.PORT || 3000;
